0a71585ff9204c4a6de938ec9ec0eecab65851f5
Anna Khvistik <anna.khvistik@monterosa.co.uk>
2020-06-04 20:56:32 +0300

    add soultion for 6-module 2-task

--- 6-module/2-task/controllers/categories.js
+++ 6-module/2-task/controllers/categories.js
@@ -1,3 +1,17 @@
+const Category = require('../models/Category');
+
+const mapSubCategory = ({title, _id}) => ({
+  id: _id,
+  title,
+});
+
+const mapCategory = ({title, subcategories, _id}) => ({
+  id: _id,
+  title,
+  subcategories: subcategories.map(mapSubCategory),
+});
+
 module.exports.categoryList = async function categoryList(ctx, next) {
-  ctx.body = {categories: []};
+  const categories = await Category.find();
+  ctx.body = {categories: categories.map(mapCategory)};
 };
