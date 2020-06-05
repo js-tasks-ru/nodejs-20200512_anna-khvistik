54db79b1cd16de767a9fd8660a2d86af4a62140b
Anna Khvistik <anna.khvistik@monterosa.co.uk>
2020-06-05 16:56:54 +0300

    add solution for 6-module 3-task

--- 6-module/3-task/controllers/products.js
+++ 6-module/3-task/controllers/products.js
@@ -1,3 +1,28 @@
+const Product = require('../models/Product');
+
+const mapProduct = ({
+  category,
+  description,
+  images,
+  price,
+  subcategory,
+  title,
+  _id,
+}) => {
+  return {
+    category,
+    description,
+    images,
+    price,
+    subcategory,
+    title,
+    id: _id,
+  };
+};
+
 module.exports.productsByQuery = async function productsByQuery(ctx, next) {
-  ctx.body = {products: []};
+  const {query} = ctx.request.query;
+  const products = await Product.find({$text: {$search: query}});
+
+  ctx.body = {products: products.map(mapProduct)};
 };
