9c824203cfc34fe42794a3dbf54f003a3f1261ce
Anna Khvistik <anna.khvistik@monterosa.co.uk>
2020-06-27 12:49:49 +0300

    add solution for 8-module 2-task

--- 8-module/2-task/controllers/orders.js
+++ 8-module/2-task/controllers/orders.js
@@ -1,8 +1,30 @@
 const Order = require('../models/Order');
 const sendMail = require('../libs/sendMail');
+const mapOrder = require('../mappers/order');
 
 module.exports.checkout = async function checkout(ctx, next) {
+  const {product, phone, address} = ctx.request.body;
+  const order = await Order.create({
+    product,
+    phone,
+    address,
+    user: ctx.user.id,
+  });
+
+  await sendMail({
+    template: 'order-confirmation',
+    locals: {product, id: order.id},
+    to: ctx.user.email,
+    subject: 'Заказ',
+  });
+
+  ctx.body = {
+    order: order.id,
+  };
 };
 
 module.exports.getOrdersList = async function ordersList(ctx, next) {
+  const orders = await Order.find({user: ctx.user.id}).populate('product');
+
+  ctx.body = {orders: orders.map(mapOrder)};
 };
--- 8-module/2-task/models/Order.js
+++ 8-module/2-task/models/Order.js
@@ -2,6 +2,32 @@ const mongoose = require('mongoose');
 const connection = require('../libs/connection');
 
 const orderSchema = new mongoose.Schema({
+  user: {
+    required: true,
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+  },
+  product: {
+    required: true,
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Product',
+  },
+  phone: {
+    required: true,
+    type: String,
+    validate: [
+      {
+        validator(value) {
+          return /\+?\d{6,14}/.test(value);
+        },
+        message: 'Неверный формат номера телефона.',
+      },
+    ],
+  },
+  address: {
+    required: true,
+    type: String,
+  },
 });
 
 module.exports = connection.model('Order', orderSchema);
